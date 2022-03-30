fbe7388163882fc04399f03488c833766b14e425
Jon Flitcroft <jondenise@gmail.com>
2022-03-30 22:49:26 +0100

    Styled main layout

---
+++ script.js
@@ -0,0 +1,11 @@
+let library = [];
+
+function Book(title, author, category, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.category = category;
+  this.pages = pages;
+  this.read = read;
+}
+
+const addBook = () => {};
